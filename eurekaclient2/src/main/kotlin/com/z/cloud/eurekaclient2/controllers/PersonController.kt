5f8c4dd00a2a53fbc5f76c0533a583d26b255933
Mariano Lopez <m_villa@hotmail.com>
2018-06-19 12:49:07 -0300

    update properties

--- eurekaclient2/src/main/kotlin/com/z/cloud/eurekaclient2/controllers/PersonController.kt
+++ eurekaclient2/src/main/kotlin/com/z/cloud/eurekaclient2/controllers/PersonController.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/feignPerson")
 class PersonController(private val personFeignService: PersonFeignService) {
     @GetMapping fun getAll(): List<Person> = personFeignService.getAll()
+
     @GetMapping("/{name}") fun getByName(@PathVariable name:String): List<Person> = personFeignService.getByName(name)
     @GetMapping("/byId/{id}") fun getById(@PathVariable id:String): Person? = personFeignService.getById(id)
     @PostMapping fun add(@RequestBody person: Person): Boolean = personFeignService.add(person)
--- eurekaclient2/src/main/kotlin/com/z/cloud/eurekaclient2/data/DataClasses.kt
+++ eurekaclient2/src/main/kotlin/com/z/cloud/eurekaclient2/data/DataClasses.kt
@@ -1,6 +1,7 @@
 package com.z.cloud.eurekaclient2.data
 
 data class Person(var id:String?,val name:String){
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other?.javaClass != javaClass) return false
